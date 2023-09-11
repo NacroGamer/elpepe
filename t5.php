4e6e59b82709bcdb5d9877469ebe9162c5d970fa
NacroGamer <136621129+NacroGamer@users.noreply.github.com>
2023-09-11 10:59:04 -0600

    Add files via upload

---
+++ t5.php
@@ -0,0 +1,195 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+<fieldset>
+        <form name="examenteorico">
+            <!--Presgunta 1-->
+            <h3>EXAMEN TEORICO DE DIAGNOSTICO TABLA DE TIPO DE POKEMON</h3><br>
+            <div class="contenedornombre">
+                <label for="nombre"></label>INGRESA TU NOMBRE
+                <input type="text" name="nombre" id="nombre"><br>
+                <label for="grupo"></label>INGRESA TU GRUPO
+                <input type="text" name="grupo" id="grupo">
+            </div>
+
+            <label for="efectividad">¿Cual es el mejor personaje de Smash?</label><br>
+            <div class="acomodo"><input type="radio" name="efectividad" value="agua" id="agua">El nueves
+
+
+
+                <input type="radio" name="efectividad" value="tierra" id="tierra">Terry
+
+
+                <input type="radio" name="efectividad" value="planta">Pyra y Mythra
+
+
+
+                <input type="radio" name="efectividad" value="volador">Rey dede
+            </div>
+
+
+            <br><br><br>
+            <!--Presgunta 2-->
+            <label for="p2">¿Cual es el personaje de entrenador pokemon que sale en Smash Bros?</label><br>
+            <div class="acomodo"><input type="radio" name="p2" value="go"><img src="https://upload.wikimedia.org/wikipedia/pt/7/7f/Red_pokemon.png" alt="">
+
+
+
+                <input type="radio" name="p2" value="bruck"><img src="https://images.wikidexcdn.net/mwuploads/wikidex/thumb/3/3b/latest/20180812021535/Brock_LGPE.png/375px-Brock_LGPE.png" alt="">
+
+
+                <input type="radio" name="p2" value="misty"><img src="https://images.wikidexcdn.net/mwuploads/wikidex/thumb/f/fd/latest/20180812024731/Misty_LGPE.png/375px-Misty_LGPE.png" alt="">
+
+
+                <input type="radio" name="p2" value="ash"><img
+                    src="https://images.wikidexcdn.net/mwuploads/wikidex/thumb/0/02/latest/20221113131941/Ash_%28Viajes_Pok%C3%A9mon%29_2.png/375px-Ash_%28Viajes_Pok%C3%A9mon%29_2.png"
+                    alt="">
+            </div>
+            <br><br><br>
+
+            <!--Presgunta 3-->
+            <label for="p3">¿Como se hace parry?</label><br>
+            <div class="acomodo"><input type="radio" name="p3" value="electrico">Pulsando Zl y manteniendo
+
+
+
+                <input type="radio" name="p3" value="acero">Pulsando Zr y manteniendo
+
+
+
+                <input type="radio" name="p3" value="planta">Pulsando Zl y quitando
+
+
+
+                <input type="radio" name="p3" value="veneno">Pulsando Zr y quitando
+            </div>
+            <br><br><br>
+
+            <!--Presgunta 4-->
+            <label for="p4">¿Cuantos DLC hay?</label><br>
+            <div class="acomodo"><input type="radio" name="p4" value="siniestro">99
+
+
+                <input type="radio" name="p4" value="veneno">5
+
+                <input type="radio" name="p4" value="normal">17
+
+                <input type="radio" name="p4" value="lucha">38
+            </div>
+            <br><br><br>
+
+            <!--Presgunta 5-->
+            <label for="p5">¿Cuales personajes carrean mas?</label><br>
+            <div class="acomodo"><input type="radio" name="p5" value="acero">Pyra y Mythra
+
+
+                <input type="radio" name="p5" value="volador">El nueves
+
+
+
+                <input type="radio" name="p5" value="fuego">Steve
+
+
+
+                <input type="radio" name="p5" value="planta">Joker
+            </div>
+            <br><br><br>
+
+            <!--Presgunta 6-->
+            <label for="p6">¿Que personajes tienen arco?</label><br>
+            <div class="acomodo"><input type="radio" name="p6" value="snorlax">Byleth
+
+
+                <input type="radio" name="p6" value="cosmog">Toon Link
+
+
+                <input type="radio" name="p6" value="pikachu">Samus
+
+
+
+                <input type="radio" name="p6" value="gengar">El nueves
+            </div>
+            <br><br><br>
+
+
+            <!--Presgunta 7-->
+            <label for="p7">¿En que personaje de daño se activa el GO! en Terry?</label><br>
+            <div class="acomodo"><input type="radio" name="p7" value="planta">99.9%
+
+
+                <input type="radio" name="p7" value="bicho">100%
+
+
+                <input type="radio" name="p7" value="veneno">99%
+
+
+
+                <input type="radio" name="p7" value="normal">75%
+            </div>
+            <br><br><br>
+
+
+            <!--Presgunta 8-->
+            <label for="p8">¿Que personajes se pueden hacer piedra?</label><br>
+            <div class="acomodo"><input type="radio" name="p8" value="bicho">Kirby
+
+
+                <input type="radio" name="p8" value="fuego">Snake
+
+
+                <input type="radio" name="p8" value="planta">Mario
+
+
+
+                <input type="radio" name="p8" value="acero">Samus
+            </div>
+            <br><br><br>
+
+
+            <!--Presgunta 9-->
+            <label for="p9">¿Que personaje no aparece en Smash bros?</label><br>
+            <div class="acomodo"><input type="radio" name="p9" value="siniestro">Waluigi
+
+
+                <input type="radio" name="p9" value="hielo">Mario
+
+
+                <input type="radio" name="p9" value="fuego">Toad
+
+
+
+                <input type="radio" name="p9" value="fantasma">Roblox
+            </div>
+            <br><br><br>
+
+
+            <!--Presgunta 10-->
+            <label for="p0">¿Que personaje es DLC?</label><br>
+            <div class="acomodo"><input type="radio" name="p0" value="ocaso">Sonic
+
+
+                <input type="radio" name="p0" value="ultra">Mario
+
+                <input type="radio" name="p0" value="master">Snake
+
+
+
+                <input type="radio" name="p0" value="acopio">Joker
+            </div>
+            <br><br><br>
+            
+
+           
+
+            <button>Enviar
+        </form>
+    </fieldset>
+</body>
+</html>
